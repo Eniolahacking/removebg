7137e2e88ec68b5ed1f32222864da20a8f254e68
MY REAL NAME IS INVISIBLE <93032268+Eniolahacking@users.noreply.github.com>
2022-10-14 17:19:08 +0100

    Update exce.py

--- exce.py
+++ exce.py
@@ -3,5 +3,5 @@ import os
 fileo=r'/storage/emulated/0/Donwload/Data Bleanding File Two.xlsx'
 you=pandas.read_excel(fileo)
 print(you)
-print(you.loc[[1:6],["BONUS"]])
+print(you.loc[[1,6],["BONUS"]])
 os.startfile(r'/storage/emulated/0/Donwload/Data Bleanding File Two.xlsx')
