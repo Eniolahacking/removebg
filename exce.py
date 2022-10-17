bdc76f9ea22ecbcd45ac44b1db9b90e8a106ed03
MY REAL NAME IS INVISIBLE <93032268+Eniolahacking@users.noreply.github.com>
2022-10-17 21:12:05 +0100

    Add files via upload

---
+++ exce.py
@@ -0,0 +1,19 @@
+from PIL import Image
+import math
+from colormaa import fore
+y=input('Put the file dircetiom e.g r file name')
+ao=Image.open(y)
+ww,hh=ao.size
+print(fore.BLUE+ao.size,'the size pf the image by')
+Osize=input('x as width eg 300')
+Wsize=y=input('y as height eg 100')
+try:
+	fil=ao.resize((Osize,Wsizr),Image.ANTIALIAS)
+print(fore.YELLOW+'IN PROGRESS PLEASE WAIT')
+loc=input(fore.BLUE+f'where to save the image if \nit was jpg or png you want to save it input remember to put r')
+fil.save(loc,quality=95)
+Except :
+	print(f'was not found',y+fore.RED)
+	finally:
+		print('finished')
+		ao.close()
