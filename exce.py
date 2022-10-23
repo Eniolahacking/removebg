93b0d7cb05c1b960cd7a3727b4e33c6206c1e906
MY REAL NAME IS INVISIBLE <93032268+Eniolahacking@users.noreply.github.com>
2022-10-23 21:17:55 +0100

    Update exce.py

--- exce.py
+++ exce.py
@@ -1,18 +1,7 @@
-from PIL import Image
-import math
-from colormaa import fore
-y=input('Put the file dircetiom e.g r file name')
-ao=Image.open(y)
-ww,hh=ao.size
-print(fore.BLUE+ao.size,'the size pf the image by')
-Osize=input('x as width eg 300')
-Wsize=y=input('y as height eg 100')
-try:
-	fil=ao.resize((Osize,Wsizr),Image.ANTIALIAS)
-loc=input('where to save the image if it was jpg or png you want to save it input rememberto put r')
-fil.save(loc,quality=95)
-Except :
-	print('was not found',y+fore.RED)
-	finally:
-		print('finished')
-		ao.close()
+
+import mechanize 
+def url(site):
+serve=mechanize.Browser()
+you=server.open(site) 
+pon=you.read() 
+print pon url("https://www.w3schools.com/https")
