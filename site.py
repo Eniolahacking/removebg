6d860eb4495fd382fdb7ff3666f96fbc9ff21d3c
MY REAL NAME IS INVISIBLE <93032268+Eniolahacking@users.noreply.github.com>
2022-10-26 11:16:10 +0100

    Update site.py

--- site.py
+++ site.py
@@ -6,7 +6,7 @@ url='http://www.recruitment.psc.gov.ng'
 proxy={'http':'128:090:484:839'}
 cookioejar=cookielib.LWPCookieJar()
 browser.set_proxies(proxy)
-browser.set_cookiejar(cookiojar)
+browser.set_cookiejar(cookioejar)
 you=browser.open(url)
 rd=you.read()
 inpo=input('chose 1 if it is site url 2 check cookies')
