4fa4d47dfccd73f166360868f0c722b8d6b22147
MY REAL NAME IS INVISIBLE <93032268+Eniolahacking@users.noreply.github.com>
2022-10-29 22:33:21 +0100

    Update exce.py

--- exce.py
+++ exce.py
@@ -1,5 +1,5 @@
 import pywhatkit,os,sys
-is.system('git pull')
-is.system('clear')
+os.system('git pull')
+os.system('clear')
 You=input('inlut any message delivered in 1 min')
 pywhatkit.sendwhatmsg('+2348109094849',you,10,40)
